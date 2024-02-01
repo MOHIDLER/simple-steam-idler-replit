16b16ecfd2980ca7af3f897cc48ccb97c8cfc757
MOHIDLER <135294532+MOHIDLER@users.noreply.github.com>
2024-02-01 00:08:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
-var status = 1;  // 1 - online, 7 - invisible
+var games = [730, 304930, 42700, 1172620, 289650, 42710, 10, 282070, 105600, 322500, 213670, 21690, 254700, 418370, 108600, 620, 400, 218620, 239030, 550, 500, 12120, 12110, 12100, 4000, 242760, 239140, 454650, 251570, 1012880, 35140, 200260];  // Enter here AppIDs of the needed games
+var status = 7;  // 1 - online, 7 - invisible
 
 
 user = new steamUser();
