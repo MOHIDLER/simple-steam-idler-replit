f45cf219977397918ccd9b5a29a827c1a328c791
MOHIDLER <135294532+MOHIDLER@users.noreply.github.com>
2024-02-19 02:28:22 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ var password = process.env.password;
 var shared_secret = process.env.shared;
 
 var games = [730, 70, 304930, 440, 570];  // Enter here AppIDs of the needed games
-var status = 7;  // 1 - online, 7 - invisible
+var status = 1;  // 1 - online, 7 - invisible
 
 
 user = new steamUser();
-user.logOn({"accountName": username, "password": password, "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
+user.logOn({"accountName": username, "password": password,
 user.on('loggedOn', () => {
 	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
 	user.setPersona(status);               
@@ -24,11 +24,11 @@ var password2 = process.env.password2;
 var shared_secret2 = process.env.shared2;
 
 var games2 = [730, 1938090, 304930, 440, 1172470, 578080, 570];  // Enter here AppIDs of the needed games
-var status2 = 7;  // 1 - online, 7 - invisible
+var status2 = 1;  // 1 - online, 7 - invisible
 
 
 user2 = new steamUser();
-user2.logOn({"accountName": username2, "password": password2, "twoFactorCode": steamTotp.generateAuthCode(shared_secret2)});
+user2.logOn({"accountName": username2, "password": password2, 
 user2.on('loggedOn', () => {
 	if (user2.steamID != null) console.log(user2.steamID + ' - Successfully logged on');
 	user2.setPersona(status2);               
